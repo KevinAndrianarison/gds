5bc2a14ba0188840c7e418296b756f23995f99d2
KevinAndrianarison <kevinandrianarison25@gmail.com>
2025-04-18 18:07:09 +0300

    load login

--- src/composants/Inputtext.jsx
+++ src/composants/Inputtext.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import '../styles/Inputlogin.css'
 
 export default function Inputtext() {
   return (
-    <div>Inputtext</div>
-  )
+    <div className="wrapper">
+      <div className="inputlogin">
+        <input
+          placeholder="Entrer votre email"
+          className="inputlogin--input"
+          type='email'
+        />
+      </div>
+    </div>)
 }
