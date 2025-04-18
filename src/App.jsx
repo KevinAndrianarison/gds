d3e245a23891aa3cd94f2e63c8bad68433c237d2
KevinAndrianarison <kevinandrianarison25@gmail.com>
2025-04-18 10:53:34 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
     <div>
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger>Is it?</AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
