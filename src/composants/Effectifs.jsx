32cbaac53de4b72597c11bc6d4aa7a9a4e059509
KevinAndrianarison <kevinandrianarison25@gmail.com>
2025-05-11 12:31:18 +0300

    feat: Affichage du nombre réel d'ACLs et d'utilisateurs dans le composant Effectifs

--- src/composants/Effectifs.jsx
+++ src/composants/Effectifs.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import UserBlockEffectif from './UserBlockEffectif'
+import { UserContext } from '@/contexte/useUser'
 
 export default function Effectifs() {
+    const { users, getAlluser } = useContext(UserContext);
+
+    useEffect(() => {
+        getAlluser();
+    }, []);
+
+    const aclCount = users ? users.filter(user => user.role === 'acl').length : 0;
+    const userCount = users ? users.filter(user => user.role === 'user').length : 0;
+
     return (
         <div>
             <h1 className='uppercase text-xs font-bold mt-8'>Effectifs</h1>
             <div className='flex flex-wrap gap-4 mt-4'>
-                <UserBlockEffectif role='ACL(s)' effectif={2} imgClassName='logoACL' />
-                <UserBlockEffectif role='Utilisateurs' effectif={10} imgClassName='logoUtilisateur' />
+                <UserBlockEffectif role='ACL(s)' effectif={aclCount} imgClassName='logoACL' />
+                <UserBlockEffectif role='Utilisateurs' effectif={userCount} imgClassName='logoUtilisateur' />
             </div>
         </div>)
 }
